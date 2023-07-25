330ba22af4811034599dbef9115a1205352fd874
Aaron Beckley <45105699+ambeckley@users.noreply.github.com>
2023-07-25 15:37:23 -0400

    Add files via upload

---
+++ bitmapTest/bitmapTest/bitmapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitmapTestApp.swift
+//  bitmapTest
+//
+//  Created by Aaron Beckley on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct bitmapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
